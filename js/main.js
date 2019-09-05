736964a6f8972d44a6c1982027af50de7826b678
Søren Ovesen <eaasbo@students.eaaa.dk>
2019-09-05 13:07:34 +0200

    Get locations; changed terminology a little

--- js/main.js
+++ js/main.js
@@ -3,47 +3,58 @@
 let sheetId = "1Ifk-tlxehniw0rZ66jm7lf4bWb27jjfbiIXhHt2Lgbg";
 let sheetNumber = 1;
 let sheetUrl = `https://spreadsheets.google.com/feeds/list/${sheetId}/${sheetNumber}/public/full?alt=json`;
-let animalList = [];
+let petList = [];
 // console.log(sheetUrl);
 
+// Get the list of pets from our Google Sheet
 fetch(sheetUrl)
   .then(function(response) {
     return response.json();
   })
   .then(function(json) {
-    console.log(json);
-    appendAnimals(json.feed.entry);
-    getSpecies(json.feed.entry);
+    petList = json.feed.entry;
+    console.log(petList);
+    appendPets(petList);
+    getSpecies(petList);   // Don't need these here specifically, it's just to test them
+    getLocations(petList); //
   });
 
-function appendAnimals(animals)
-{
+function appendPets(pets) {
   let htmlTemplate = "";
 
-  for (let animal of animals)
-  {
+  // Basic DOM insertion to check if it works
+  for (let pet of pets) {
     htmlTemplate += `
-    <article>
-      <img src="${animal["gsx$photo"]["$t"]}">
-      <h2>${animal["gsx$name"]["$t"]}</h2>
-      <p>Age: ${animal["gsx$age"]["$t"]}</p>
-      <p>Location: ${animal["gsx$place"]["$t"]}
-    </article>
+      <article>
+        <img src="${pet["gsx$photo"]["$t"]}">
+        <h2>${pet["gsx$name"]["$t"]}</h2>
+        <p>Age: ${pet["gsx$age"]["$t"]}</p>
+        <p>Location: ${pet["gsx$location"]["$t"]}
+      </article>
     `;
   }
 
-  document.querySelector("#animal-container").innerHTML = htmlTemplate;
+  document.querySelector("#pet-container").innerHTML = htmlTemplate;
 }
 
-function getSpecies(animals)
-{
+function getSpecies(pets) {
   let speciesArray = [];
 
-  for (let animal of animals)
-  {
-    if (speciesArray.includes(animal["gsx$type"]["$t"].toLowerCase()) === false)
-    {
-      speciesArray.push(animal["gsx$type"]["$t"].toLowerCase())
+  for (let pet of pets) {
+    if (speciesArray.includes(pet["gsx$type"]["$t"].toLowerCase()) === false) {
+      speciesArray.push(pet["gsx$type"]["$t"].toLowerCase())
+    }
+  }
+
+  console.log(speciesArray);
+}
+
+function getLocations(pets) {
+  let speciesArray = [];
+
+  for (let pet of pets) {
+    if (speciesArray.includes(pet["gsx$location"]["$t"].toLowerCase()) === false) {
+      speciesArray.push(pet["gsx$location"]["$t"].toLowerCase())
     }
   }
 
